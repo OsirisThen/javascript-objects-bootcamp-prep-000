c5c02998ae98081f1d3efcd709ecbfc5e0c38129
Osiris Then <osiriscuevas@hotmail.com>
2018-08-30 12:27:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -12,7 +12,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 function removeFromPlaylist(playlist, artistName){
 
   delete playlist[artistName];
-  //delete playlist.artistName['Slowdive'];
   return playlist;
 
 }
